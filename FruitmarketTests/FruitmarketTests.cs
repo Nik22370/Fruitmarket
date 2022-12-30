efa6254505b85c69c1f8018ebac9234e76c85099
pa4ul <paulherbich1@gmail.com>
2022-12-30 21:25:12 +0100

    Fruitmarket Testcases

--- FruitmarketTests/FruitmarketTests.cs
+++ FruitmarketTests/FruitmarketTests.cs
@@ -9,13 +9,16 @@ public class UnitTest1
     {
         
             // ARRANGE
-            var Market = new Market("Markt 01", new Address("Musterstrasse 1", "22"), new Owner());
+            var Owner1 = new Owner(new Person("max","mustermann",new Address("Musterstrasse","1")),"Musterstand");
+            var Owner2 = new Owner(new Person("michael","mustertyp",new Address("Gasse Am Weg","11")),"Musterstand");
+
+            var Market1 = new Market("Markt 01", new Address("Musterstrasse 1", "22"), Owner1);
 
             // ACT
-            Market.changeAddress(new Address("Musterstrasse 2","22"));
+            Market1.changeOwner(Owner2);
         
             // ASSERT
-            Assert.Equal("Musterstrasse 2", Market.Address.Street);
+            Assert.Equal("michael", Owner2.Firstname);
         
     }
     [Fact]
@@ -23,7 +26,8 @@ public class UnitTest1
     {
         
         // ARRANGE
-        var Market = new Market("Markt 01", new Address("Musterstrasse 1", "22"), new Owner());
+        var Owner1 = new Owner(new Person("max","mustermann",new Address("Musterstrasse","1")),"Musterstand");
+        var Market = new Market("Markt 01", new Address("Musterstrasse 1", "22"), Owner1);
 
         // ACT
         Market.changeAddress(new Address("Musterstrasse 2","22"));
@@ -36,21 +40,36 @@ public class UnitTest1
     public void test_addProducts()
     {
         
+        var Owner1 = new Owner(new Person("max","mustermann",new Address("Musterstrasse","1")),"Musterstand");
+        var Market = new Market("Markt 01", new Address("Musterstrasse 1", "22"), Owner1);
+        var Product1 = new Product("Basket", 14.99M);
+        var Product2 = new Product("Pillow", 20.00M);
+
+        // ACT
+        Market.addProducts(Product1);
+        Market.addProducts(Product2);
         
+        // ASSERT
+        Assert.Equal(2, Market.Products.Count);
         
     }
     [Fact]
     public void test_removeProducts()
     {
         
+        var Owner1 = new Owner(new Person("max","mustermann",new Address("Musterstrasse","1")),"Musterstand");
+        var Market = new Market("Markt 01", new Address("Musterstrasse 1", "22"), Owner1);
+        var Product1 = new Product("Basket", 14.99M);
+        var Product2 = new Product("Pillow", 20.00M);
+
+        // ACT
+        Market.addProducts(Product1);
+        Market.addProducts(Product2);
+        Market.removeProducts(Product1);
         
+        // ASSERT
+        Assert.Equal(1, Market.Products.Count);
         
     }
-    [Fact]
-    public void test_countProducts()
-    {
-        
-        
-        
-    }
+   
 }
